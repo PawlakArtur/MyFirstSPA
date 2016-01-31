b46a40dec9686ea236ec3335999c8ac2a6955b7f
Artur Pawlak <pawlak.artur90@gmail.com>
2016-01-31 17:03:29 +0100

    function enter

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,12 @@
+function enter(url){
+	$('#content').load(url);
+}
+
 $(window).on('hashchange', function(){
 	var hash = location.hash.substr(1);
 	var page = 'templates/' + hash + '.html';
-	$('#content').load(page);
+
+	enter(page);
 });
+
+enter('templates/home.html');
